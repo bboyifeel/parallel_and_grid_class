171c321ad694aa54e0f2c729b41f3f3b1cf768e3
Igor <bboyifeel@gmail.com>
2019-11-28 19:50:30 +0100

    monte carlo fix

--- MPI_Pi/3_seq.cpp
+++ MPI_Pi/3_seq.cpp
@@ -2,40 +2,35 @@
 #include <random>
 #include <cmath>
 
-const int MAX_NB_RANDOM_POINT = 1000;
-
 void runMonteCarloPiCalc();
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
 	runMonteCarloPiCalc();
 	return 0;
 }
 
-void runMonteCarloPiCalc()
-{
+void runMonteCarloPiCalc() {
 	int ncirc = 0;
 	double r = 1.0;
 	double pi, x, y;
-	double accuracy = 0.0001;
 	int i = 0;
-	double error = M_PI;
+	unsigned int MAX_NB_RANDOM_POINT = 1000;
 
 	std::random_device	rand_dev;
 	std::default_random_engine generator(rand_dev());
 	std::uniform_real_distribution<double> distribution(0.0, 1.0);
 
-	std::cout << "Enter desired accuracy (format e=0.0001)" << std::endl;
-	std::cin >> accuracy;
+	std::cout << "Enter desired number of dots to generate" << std::endl;
+	std::cin >> MAX_NB_RANDOM_POINT;
 
-	for (i = 1; i < MAX_NB_RANDOM_POINT && error > accuracy; i++)
+	for (i = 0; i < MAX_NB_RANDOM_POINT; i++)
 	{
 		x = distribution(generator) * r * 2.0 - r;
 		y = distribution(generator) * r * 2.0 - r;
 		if ((x * x + y * y) <= r * r)
 			ncirc++;
-		pi = 4.0 * ((double) ncirc / (double)i);
-		error = std::abs(M_PI - pi);
 	}
+
+	pi = 4.0 * ((double) ncirc / (double)i);
 	printf("PI ~ %.20f\n", pi);
 }
