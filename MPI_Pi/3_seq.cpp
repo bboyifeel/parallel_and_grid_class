a074b0a291bffa32fe98c9693f80a9c076fbe07f
Igor <bboyifeel@gmail.com>
2019-11-29 11:01:11 +0100

    epsilon 3_1

--- MPI_Pi/3_seq.cpp
+++ MPI_Pi/3_seq.cpp
@@ -25,16 +25,17 @@ void runMonteCarloPiCalc() {
 	int ncirc = 0;
 	double r = 1.0;
 	double pi, x, y;
-	int i = 0;
 	unsigned int MAX_NB_RANDOM_POINT = 1000;
+	double epsilon = 0.00001;
+
+	do {
+		std::cout << "Enter desired accuracy (format e=0.0001)" << std::endl;
+		std::cin >> epsilon;
+	} while (epsilon >= 1 || epsilon <= 0);
 
 	std::random_device	rand_dev;
 	std::default_random_engine generator(rand_dev());
 	std::uniform_real_distribution<double> distribution(0.0, 1.0);
-
-	std::cout << "Enter desired number of dots to generate" << std::endl;
-	std::cin >> MAX_NB_RANDOM_POINT;
-
 
 	std::string inFileName = "insideOfCircle.dat";
 	std::string outFileName = "outsideOfCircle.dat";
@@ -46,24 +47,33 @@ void runMonteCarloPiCalc() {
 	std::ofstream outsideOfCircle;
   	outsideOfCircle.open (outFileName);
 
-
-	for (i = 0; i < MAX_NB_RANDOM_POINT; i++)
-	{
-		x = distribution(generator) * r * 2.0 - r;
-		y = distribution(generator) * r * 2.0 - r;
-		if ((x * x + y * y) <= r * r) {
-			ncirc++;
-			insideOfCircle << x << " " << y << std::endl;
+  	int iter = 0;
+  	bool done = false;
+  	double error = 1;
+  	
+  	while (error > epsilon) {
+  		iter++;
+		for (int i = 0; i < MAX_NB_RANDOM_POINT; i++)
+		{
+			x = distribution(generator) * r * 2.0 - r;
+			y = distribution(generator) * r * 2.0 - r;
+			if ((x * x + y * y) <= r * r) {
+				ncirc++;
+				insideOfCircle << x << " " << y << std::endl;
+			}
+			else {
+				outsideOfCircle << x << " " << y << std::endl;
+			}
 		}
-		else {
-			outsideOfCircle << x << " " << y << std::endl;
-		}
+		pi = 4.0 * ((double) ncirc / ((double) MAX_NB_RANDOM_POINT * iter));
+		error = std::abs(M_PI - pi);
 	}
 
 	insideOfCircle.close();
 	outsideOfCircle.close();
 
-	pi = 4.0 * ((double) ncirc / (double)i);
+	std::cout << "Number of iteration " << iter << std::endl;
+	std::cout << "epsilon " << epsilon << std::endl; 
 	printf("PI ~ %.20f\n", pi);
 }
 
